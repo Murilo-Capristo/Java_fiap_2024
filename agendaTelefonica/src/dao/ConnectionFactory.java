50ebf217592599ed0c3ef3956e2c7b21557e3e21
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-23 07:28:11 -0300

    Projeto

--- agendaTelefonica/src/dao/ConnectionFactory.java
+++ agendaTelefonica/src/dao/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
         Connection conexao = null;
         try{
             conexao = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl",
-                    "pf1551", "fiap23");
+                    "pf1551", "senha");
         }catch (SQLException erro){
             erro.printStackTrace();
         }
